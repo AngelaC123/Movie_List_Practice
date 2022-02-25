2b0213a0d5421738de5f54823f5f29f4b9bad725
AngelaC123 <angelarare@gmail.com>
2022-02-25 16:42:33 +0800

    Required handlebars and set up views

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // Require express
-const express =  require('express')
-const app =  express()
+const express = require('express')
+const app = express()
 
 // Set server variable
 const port = 3000
 
+// Require handlebars
+const exphbs = require('express-handlebars')
+
+// Set template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 // Set route
-app.get('/', (req,res) => {
-  res.send(`<h2>This is my movie list build with express! </h2>`)
+app.get('/', (req, res) => {
+  res.render('index')
 })
 
 // Listen to the server
